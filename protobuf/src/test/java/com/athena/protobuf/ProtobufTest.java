a169f921d65dea7efe643a5f9671257fe2d2ebd9
ghostjialong <ghostjialong@gmail.com>
2017-11-07 00:28:44 +0800

    some test code for protobuf

---
+++ protobuf/src/test/java/com/athena/protobuf/ProtobufTest.java
@@ -0,0 +1,23 @@
+package com.athena.protobuf;
+
+/**
+ * Created by wangjialong on 11/6/17.
+ */
+public class ProtobufTest {
+
+    public static void main(String[] args) throws Exception{
+        MessageEntity.Message message = MessageEntity.Message.newBuilder()
+                .setMessageId("105001676")
+                .setToUserId("105002955")
+                .setBody("Hello, I wanna chat with you.....").build();
+        //将对象转译成字节数组,序列化
+        byte[] messageByteArray = message.toByteArray();
+        //将字节数组转译成对象,反序列化
+        MessageEntity.Message revceivedMsg = MessageEntity.Message.parseFrom(messageByteArray);
+
+        System.out.println(message.getMessageId());
+        System.out.println(messageByteArray);
+        System.out.println(revceivedMsg.toString());
+    }
+
+}
